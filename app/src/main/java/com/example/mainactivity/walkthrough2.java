81b5be143cfb60103502403dc626f91b9a8a4484
emirhankurtulus <kurtulusemirhan53@gmail.com>
2022-01-09 05:48:01 +0300

    First day
    I'm creating my final exam project of my mobile apps course. This summit has just UI. I downloaded that from https://www.xdguru.com/topnote-free-xd-mobile-ui-kit/

--- app/src/main/java/com/example/mainactivity/walkthrough2.java
+++ app/src/main/java/com/example/mainactivity/walkthrough2.java
@@ -6,17 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class walkthrough2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_walkthrough2);
     }
     public void skip (View View)
     {
-        Intent ikincisayfagecis=new Intent(MainActivity.this, walkthrough2.class);
+        Intent ikincisayfagecis=new Intent(walkthrough2.this, walkthrough3.class);
         startActivity(ikincisayfagecis);
     }
 }
--- app/src/main/java/com/example/mainactivity/walkthrough3.java
+++ app/src/main/java/com/example/mainactivity/walkthrough3.java
@@ -6,17 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class walkthrough3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_walkthrough3);
     }
-    public void skip (View View)
+    public void sign (View View)
     {
-        Intent ikincisayfagecis=new Intent(MainActivity.this, walkthrough2.class);
+        Intent ikincisayfagecis=new Intent(walkthrough3.this, login.class);
         startActivity(ikincisayfagecis);
     }
 }
